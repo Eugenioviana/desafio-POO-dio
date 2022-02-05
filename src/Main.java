ca2e596fa0632427c6d8a6ef08434185106eed57
Eugenioviana <oliveiraeugenio511@gmail.com>
2022-02-04 22:04:19 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 
 
+import One.Innavation.Digital.Dominio.Bootcamp;
 import One.Innavation.Digital.Dominio.Cursos;
+import One.Innavation.Digital.Dominio.Dev;
 import One.Innavation.Digital.Dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -19,5 +21,31 @@ public class Main {
         mentoria.setDate(LocalDate.now());
         System.out.println(mentoria);
 
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Bootcamp Java");
+        bootcamp.setDescricao("Bootcam java developer");
+        bootcamp.getConteudos().add(c1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Eugenio");
+        dev.inscreverBootcamp(bootcamp);
+        Dev dev1 = new Dev();
+        System.out.println("Conteudos inscritos: Eugenio " + dev.getConteudoInscritos());
+        dev.proguedir();
+        System.out.println("Conteudos Inscritos: Eugenio " + dev.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos: Eugenio " + dev.getConteudoConcluidos());
+        System.out.println("XP: " + dev.calculaTotalXp());
+
+
+        dev1.setNome("Bruna");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Bruna: " + dev1.getConteudoInscritos());
+        dev1.proguedir();
+        System.out.println("Conteudos Incritos Bruna: " + dev1.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Bruna: " + dev1.getConteudoConcluidos());
+        System.out.println("XP: " + dev1.calculaTotalXp());
     }
+
 }
--- src/One/Innavation/Digital/Dominio/Cursos.java
+++ src/One/Innavation/Digital/Dominio/Cursos.java
@@ -1,43 +1,27 @@
 package One.Innavation.Digital.Dominio;
 
-public class Cursos {
-    private String titulo;
-    private String descricao;
+public class Cursos extends Conteudo {
+
     private int cargaHoraria;
 
     public Cursos() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
 
     @Override
     public String toString() {
         return "Cursos{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public void setCargaHoraria(int i) {
     }
 }
